7b8fe6108f60a2987ef5110829503fafa013db7a
João Victor Pires Almeida Campos <joao.victor_55@hotmail.com>
2022-09-08 03:45:23 -0300

    fix: some layout adjusts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit, AfterContentChecked {
   {
     icon: 'person',
     label: 'Owners',
-    route: '/donos',
+    route: '/owners',
   },
   {
     icon: 'logout',
