1c6dad5f268a318e4aadcce6338ee206feaa03a7
João Victor Pires Almeida Campos <joao.victor_55@hotmail.com>
2022-09-08 01:19:22 -0300

    feat: create menu items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { LoadingService } from '../app/services/loading.service'
 import { AuthService } from './services/auth.service';
+import { Component, OnInit, ChangeDetectorRef, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterContentChecked {
   title = 'pets';
 
-  loading$: Observable<boolean>; 
+  loading$: Observable<boolean>;
 
-  constructor(private loading: LoadingService, private auth: AuthService) {}
+  menuItems = [{
+    icon: 'pets',
+    label: 'Pets',
+    route: '/pets',
+  },
+  {
+    icon: 'person',
+    label: 'Owners',
+    route: '/donos',
+  },
+  {
+    icon: 'logout',
+    label: 'Logout',
+    route: '/logout',
+    logout: true
+  }]
+
+  constructor(private loading: LoadingService, private auth: AuthService, private changeDetector: ChangeDetectorRef,) { }
+
+  ngAfterContentChecked(): void {
+    this.changeDetector.detectChanges();
+  }
 
   ngOnInit() {
     this.loading.loading$.subscribe(loading => {
